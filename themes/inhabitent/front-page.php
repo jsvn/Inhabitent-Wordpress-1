45ee8e84a583e4e3d06dd071ffb6ce5464390110
Tom Roper <Tom@Toms-MacBook-Pro-2.local>
2016-05-26 16:15:42 -0700

    finish styling custom taxonomy and journal post loops on front page

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -9,14 +9,15 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			<!-- Hero logo -->
 			<div class="front_hero">
 				<div class="hero_logo">
 				</div>
 			</div>
 
+			<!-- Taxonomy Boxes -->
 			<div class="taxonomy_box_wrapper">
-					<h1 class="home_titles">Shop Stuff</h1>
+				<h1 class="home_titles">Shop Stuff</h1>
 					<?php
 					$terms = get_terms( array(
 					'taxonomy' => 'product_type',
@@ -25,27 +26,39 @@ get_header(); ?>
 					?>
 					<?php	foreach ( $terms as $term ): ?>
 						<div class="home_taxonomy_box">
-							<img class="home_taxonomy_img" src="wp-content/themes/inhabitent/images/product-type-icons/<?php echo $term->name . '.svg'; ?>" />
-						  <p><?php	echo	$term->description; ?></p>
-							<a class="home_taxonomy_link" href="#"><?php	echo	$term->name; ?></a>
-					</div>
+							<img class="home_taxonomy_img"
+								alt="logo"
+								src="wp-content/themes/inhabitent/images/product-type-icons/<?php echo $term->slug . '.svg'; ?>" />
+						  <p class="para_tax_box"><?php	echo	$term->description; ?></p>
+							<div class="home_box_button">
+								<a class="home_taxonomy_link" href="<?php echo get_term_link($term, '$product_type') ?>"><?php	echo	$term->name; ?> Stuff</a>
+							</div>
+						</div>
 					<?php endforeach; wp_reset_postdata(); ?>
 			</div>
 
-
-			<h1 class="home_titles">Inhabitient Journal</h1>
-			<?php
-			 $args = array( 'post_type' => 'post', 'posts_per_page' => 3 );
-			 $journal_posts = get_posts( $args );
-				?>
-				<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
+			<!-- Journal Posts -->
+			<div class="home_journal_posts_wrapper">
+				<h1 class="home_titles">Inhabitient Journal</h1>
 					<?php
-						  the_title();
-						  the_date();
-						  the_excerpt();
-					 ?>
-				<?php endforeach; wp_reset_postdata(); ?>
+				 	$args = array( 'post_type' => 'post', 'posts_per_page' => 3 );
+				 	$journal_posts = get_posts( $args );
+					?>
+					<?php foreach ( $journal_posts as $post ) : setup_postdata( $post ); ?>
+						<div class="journal_box">
+							<?php	the_post_thumbnail(); ?>
+							<span class="date_comments"><?php	the_date(); ?> / <?php comments_number( '0 comments', '1 comment', '% comments' ); ?></span>
+							<h3 class="journal_home_boxes">
+								<?php the_title(); ?></h3>
+								<div class="journal_box_button">
+									<a href=<?php	the_permalink(); ?>>Read Entry</a>
+								</div>
+					 </div>
+					<?php endforeach; wp_reset_postdata(); ?>
+			</div>
 
+			<!-- Adventure Posts -->
+				<h1 class="home_titles">Latest Adventures</h1>
 
 
 
