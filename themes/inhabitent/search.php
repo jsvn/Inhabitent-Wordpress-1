b97d26980866d82e49960c04fd0dcdc552e78e17
Tom Roper <Tom@Toms-MacBook-Pro-2.local>
2016-05-29 22:08:21 -0700

    styled search results page

--- themes/inhabitent/search.php
+++ themes/inhabitent/search.php
@@ -10,12 +10,13 @@ get_header(); ?>
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
-
 			<header class="page-header">
 				<h1 class="page-title"><?php printf( esc_html( 'Search Results for: %s' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+			</header>
 
+		<?php if ( have_posts() ) : ?>
+
+		<!-- .page-header -->
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -12,20 +12,21 @@ get_header(); ?>
 
 		<?php while ( have_posts() ) : the_post(); ?>
 
-      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-      	<header class="entry-header">
-      		<?php if ( has_post_thumbnail() ) : ?>
-      			<?php the_post_thumbnail( 'large' ); ?>
-      		<?php endif; ?>
-          
-      		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-      	</header><!-- .entry-header -->
+		      <article class="single_product_wrapper" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		      	<header class="entry-header">
+		      		<?php if ( has_post_thumbnail() ) : ?>
+		      			<?php the_post_thumbnail( 'large' ); ?>
+		      		<?php endif; ?>
 
-      	<div class="entry-content">
-      		<?php the_content(); ?>
-          <?php echo CFS()->get( 'price' ); ?>
-      	</div><!-- .entry-content -->
-      </article><!-- #post-## -->
+		      	</header><!-- .entry-header -->
+
+		      	<div class="entry-content">
+							<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+						  <p class="single_product_price">	<?php echo CFS()->get( 'price' ); ?></p>
+		      		<?php the_content(); ?>
+		      	</div><!-- .entry-content -->
+		      </article><!-- #post-## -->
+
 
 
 
